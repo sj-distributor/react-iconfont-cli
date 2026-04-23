1d3f93da0392b0699b9f8bcbff08e59aab5044cc
Simon <simoonf@vip.qq.com>
2026-04-23 17:40:35 +0800

    refactor: 简化 getIconColor 函数逻辑

--- snapshots/demo-ts/helper.ts
+++ snapshots/demo-ts/helper.ts
@@ -1,12 +1,17 @@
 /* tslint:disable */
 /* eslint-disable */
 
-export const getIconColor = (color: string | string[] | undefined, index: number, defaultColor: string) => {
-  return color
-    ? (
-      typeof color === 'string'
-        ? color
-        : color[index] || defaultColor
-    )
-    : defaultColor;
+export const getIconColor = (
+  color: string | string[] | undefined,
+  index: number,
+  defaultColor: string,
+) => {
+  if (!color) return defaultColor;
+
+  // When color is a single value, only tint the first path.
+  if (typeof color === "string") {
+    return index === 0 ? color : defaultColor;
+  }
+
+  return color[index] || defaultColor;
 };
